dfce63b38ce45792cb7ce87b32e6873138f98d5e
mhakobyan <mhakobyan@griddynamics.com>
2025-09-17 23:39:31 +0400

    UserController update

--- user-service/src/main/java/com/example/controller/UserController.java
+++ user-service/src/main/java/com/example/controller/UserController.java
@@ -6,7 +6,6 @@ import com.example.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,26 +20,22 @@ public class UserController {
     }
 
     @GetMapping("/id/{id}")
-    @PreAuthorize("hasRole('ADMIN') or #id == principal")
     public ResponseEntity<UserResponse> getUserById(@PathVariable Long id) {
         return ResponseEntity.ok(userService.getById(id));
     }
 
     @PutMapping("/id/{id}")
-    @PreAuthorize("hasRole('ADMIN') or #id == principal")
     public ResponseEntity<UserResponse> updateUser(@PathVariable Long id, @Valid @RequestBody UserUpdateRequest request) {
         return ResponseEntity.ok(userService.update(id, request));
     }
 
     @DeleteMapping("/id/{id}")
-    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         userService.delete(id);
         return ResponseEntity.noContent().build();
     }
 
     @PutMapping("/id/{id}/deactivate")
-    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> deactivateUser(@PathVariable Long id) {
         userService.deactivate(id);
         return ResponseEntity.noContent().build();
